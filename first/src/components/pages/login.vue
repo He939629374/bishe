6721f7e74f4e3e64d9fb5a8643561dd183e9eaae
He939629374 <32150775+He939629374@users.noreply.github.com>
2018-02-02 17:33:58 +0800

    2.2修改
    加了图标 axiox 重写了登录页

--- first/src/components/pages/login.vue
+++ first/src/components/pages/login.vue
@@ -1,44 +1,79 @@
 <template>
-<div class="login-box" id="app" >  
- <el-row>  
-    <el-col :span="8">  
-        <el-input id="name"  v-model="name" placeholder="请输入帐号">  
-            <template slot="prepend">帐号</template>  
-        </el-input>   
-    </el-col>  
- </el-row>  
- <el-row>  
-    <el-col :span="8">  
-        <el-input id="password" v-model="password" type="password" placeholder="请输入密码">  
-            <template slot="prepend">密码</template>  
-        </el-input>  
-    </el-col>  
- </el-row>  
- <el-row>  
-    <el-col :span="8"> 
-        <router-link to="/index">
-        <el-button id="login" v-on:click="check" style="width:100%" type="primary">登录</el-button>
-        </router-link> 
-    </el-col>
-      
- </el-row>  
- <div id="no-mode-translate-demo" class="demo">
-  <div class="no-mode-translate-demo-wrapper">
-    <transition name="no-mode-translate-fade">     
-        <i class="el-icon-circle-check" v-if="isEditing" key="on" @click="isEditing = false"><span >已记住登陆</span></i>
-        <i class="el-icon-circle-check-outline" v-else key="off" @click="isEditing = true"><span>记住我</span></i  >  
-    </transition>
-  </div>
-</div>
-</div>   
+  <div class="login-box" id="app" >  
+    <el-form :model="ruleForm2"  :rules="rules2" ref="ruleForm2" >
+      <el-row>
+        <el-col :span="10">
+          <el-form-item label="" prop="name"> 
+            <el-input id="name"  v-model="ruleForm2.name" placeholder="请输入帐号"> 
+              <template slot="prepend" ><i class="el-icon-message"></i></template>
+            </el-input>   
+          </el-form-item> 
+        </el-col> 
+      </el-row>
+      <el-row>
+        <el-col :span="10">
+          <el-form-item label="" prop="password"> 
+            <el-input id="password" v-model="ruleForm2.password" type="password" placeholder="请输入密码">  
+              <template slot="prepend" ><i class="icon-lock_fill"></i></template> 
+            </el-input>  
+          </el-form-item> 
+        </el-col> 
+      </el-row>
+      <el-row>
+        <el-col :span="10">
+          <el-form-item> 
+            <!-- <router-link to="/index">        </router-link>  -->
+            <el-button id="login"  style="width:100%" type="primary" @click="submitForm('ruleForm2')">登录</el-button>
+          </el-form-item> 
+        </el-col> 
+      </el-row>
+    </el-form>
+    <div id="no-mode-translate-demo" class="demo">
+      <div class="no-mode-translate-demo-wrapper">
+        <transition name="no-mode-translate-fade">     
+          <i class="el-icon-circle-check" v-if="isEditing" key="on" @click="isEditing = false"><span >已记住登陆</span></i>
+          <i class="el-icon-circle-check-outline" v-else key="off" @click="isEditing = true"><span>记住我</span></i  >  
+        </transition>
+      </div>
+    </div>
+  </div>   
 </template>
 <script>
   export default {
     data() {
+
+      var validateName = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入帐号'));
+        } else {
+          // if (this.ruleForm2.checkPass !== '') {
+          //   this.$refs.ruleForm2.validateField('checkPass');
+          // }
+          callback();
+        }
+      };
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          callback();
+        }
+      };
       return {
         isEditing:false,
-        name:'',
-        password:''
+        ruleForm2: {
+          name: '',
+          password: '',
+        },
+        rules2: {
+          name: [
+            { validator: validateName, trigger: 'blur' }
+          ],
+          password: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+
+        }
       };
     },
     methods: {
@@ -59,8 +94,8 @@
 }
 </script>
 <style>  
-span{
-    margin-left: 8px
+.el-form-item.is-success .el-input__inner{
+      border-color:#dcdfe6
 }
 .no-mode-translate-demo-wrapper {
   position: relative;
@@ -80,13 +115,10 @@ span{
 }
 .no-mode-translate-fade-leave-active {
   transform: translateY(-31px);
-}
-.el-row {  
-    margin-bottom: 20px;  
-  }  
-    .login-box {  
-        margin-top:20%;  
-        margin-left:40%;  
-    }  
+} 
+.login-box {  
+    margin-top:20%;  
+    margin-left:40%;  
+}  
 </style>  
 
